568a22c71de2258ef42323d8e2c8dc5aacc4e8d5
kolyasademetrio <kolyasademetrio@gmail.com>
2023-02-18 00:28:34 +0200

    Selectors added

--- client/src/containers/prices/Prices.js
+++ client/src/containers/prices/Prices.js
@@ -1,21 +1,22 @@
 import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
 import Prices from '../../components/prices/Prices';
 
-function filterPrices(prices, filterBy){
+const pricesSelector = state => state.prices.items;
+const pricesFilterSelector = state => state.filter.filterBy;
+
+const pricesFiltered = createSelector([pricesSelector, pricesFilterSelector], (prices, filterBy) => {
    switch(filterBy){
-       case 'ALL':
-           return prices;
-       default:
-           return prices && prices.filter(price => price.ticker === filterBy);
-   }
-}
+      case 'ALL':
+          return prices;
+      default:
+          return prices && prices.filter(price => price.ticker === filterBy);
+  }
+});
 
 const mapStateToProps = state => {
-   const {items} = state.prices;
-   const {filterBy} = state.filter;
-
    return {
-      prices: filterPrices(items, filterBy),
+      prices: pricesFiltered(state),
    };
 };
 
